7095989f3ace6bc411e9bbb953b3b6001b3c40b9
juliebefana <92132933+juliebefana@users.noreply.github.com>
2023-06-13 22:32:51 +0000

    I replaced Nasa js file so now it works well

--- javascript/sidebarnasa.js
+++ javascript/sidebarnasa.js
@@ -13,22 +13,33 @@ fetch(nasaApiUrl)
     title.innerText = "NASA Astronomy Picture of the Day";
     title.style.marginTop = "40px";
 
-    let image = document.createElement("img");
-    image.src = data.url;
-    image.alt = "NASA APOD";
+    if (data.media_type === 'image') {
+      let image = document.createElement("img");
+      image.src = data.url;
+      image.alt = "NASA APOD";
+
+      container.appendChild(title);
+      container.appendChild(image);
+    } else if (data.media_type === 'video') {
+      let video = document.createElement("iframe");
+      video.src = data.url;
+      video.width = "100%";
+      video.height = "auto";
+      video.allowFullscreen = true;
+      video.title = "NASA APOD";
+
+      container.appendChild(title);
+      container.appendChild(video);
+    }
 
     let description = document.createElement("p");
     description.innerText = truncateDescription(data.explanation, 2);
     description.style.fontFamily = "Quicksand";
     description.style.marginTop = "10px";
 
-    container.appendChild(title);
-    container.appendChild(image);
     container.appendChild(description);
 
     sidebar[0].appendChild(container);
-
-
   })
   .catch((error) => {
     console.log('Error:', error);
